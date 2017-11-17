940104c00372fd0a56b7531b7e0c109e26413e8a
DESKTOP-TVVDN94\junse <junseol86@gmail.com>
2017-11-17 22:37:28 +0900

    ADD edit label adapter

--- app/src/main/java/ko/hyeonmin/boardpro/activities/EditLabelActivity.kt
+++ app/src/main/java/ko/hyeonmin/boardpro/activities/EditLabelActivity.kt
@@ -2,9 +2,13 @@ package ko.hyeonmin.boardpro.activities
 
 import android.app.Activity
 import android.os.Bundle
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
 import android.util.Log
 import android.view.Window
+import com.google.gson.Gson
 import ko.hyeonmin.boardpro.R
+import ko.hyeonmin.boardpro.models.Form
 import ko.hyeonmin.boardpro.utils.Caches
 
 /**
@@ -14,13 +18,22 @@ class LabelEditActivity: Activity() {
 
     var caches: Caches? = null
 
+    var editLabelRV: RecyclerView? = null
+    var editLabelLM: RecyclerView.LayoutManager? = null
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         this.requestWindowFeature(Window.FEATURE_NO_TITLE)
         setContentView(R.layout.activity_label_edit)
 
         caches = Caches(this)
-        Log.d("CACHED", caches!!.tempFormJson)
+        var form = Gson().fromJson(caches!!.tempFormJson, Form::class.java)
+
+        editLabelRV = findViewById(R.id.editLabelRV)
+        editLabelLM = LinearLayoutManager(this)
+
+        editLabelRV?.layoutManager = editLabelLM
     }
 
     override fun onPause() {
---
+++ app/src/main/java/ko/hyeonmin/boardpro/parts/Label/recyclerParts/EditLabelRCAdapter.kt
@@ -0,0 +1,5 @@
+package ko.hyeonmin.boardpro.parts.Label.recyclerParts
+
+/**
+ * Created by junse on 2017-11-17.
+ */
